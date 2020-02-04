409bd8dff9a3e8cafee906291c25acf838612cd6
Jakub Dolejs <jakubdolejs@gmail.com>
2020-02-04 13:49:41 +0000

    Switched to Android X

--- mrtdreader/src/main/java/com/appliedrec/mrtdreader/MRTDScanActivity.java
+++ mrtdreader/src/main/java/com/appliedrec/mrtdreader/MRTDScanActivity.java
@@ -11,11 +11,11 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
 import java.util.Arrays;
